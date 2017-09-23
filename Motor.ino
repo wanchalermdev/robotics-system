3de6a28574aac5baf748f09ee999515009481588
porzawk <natthachaia56@email.nu.ac.th>
2017-09-23 15:34:47 +0700

    motor

---
+++ Motor.ino
@@ -0,0 +1,20 @@
+int dir = 2;
+int pul = 3;
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(9600);
+  pinMode(dir,OUTPUT);
+  pinMode(pul,OUTPUT);
+  digitalWrite(dir,LOW);
+}
+
+void loop() {
+  for(int i=0;i<400;i++)
+  {
+   digitalWrite(pul,HIGH);
+  delay(10);
+  digitalWrite(pul,LOW);
+  delay(10);
+  }
+  delay(2000);
+}
